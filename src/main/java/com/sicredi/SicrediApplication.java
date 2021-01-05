ee6a4a20018d0c52fdfab129d38a45a585de5b55
enisonoliveira <enisonoliveira@hotmail.com>
2021-01-04 22:20:59 -0300

    Swegger

--- src/main/java/com/sicredi/SicrediApplication.java
+++ src/main/java/com/sicredi/SicrediApplication.java
@@ -3,6 +3,7 @@ package com.sicredi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 //@ComponentScan
 @SpringBootApplication
 public class SicrediApplication   {
